8296dbec82faaca28a10c71c52f1d1c5e7d5a32b
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-04-10 19:31:16 -0400

    Counter that is persistent through refresh

--- lighthall-challenge-1/src/App.js
+++ lighthall-challenge-1/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   const [clickCount, setClickCount] = useState(0);
+
+  // On mount check for preexisting count in local storage
+  useEffect(() => {
+    const storedCount = Number(localStorage.getItem("count"));
+
+    // If there is a preexisting count, set the count state to that value
+    if (storedCount) {
+      setClickCount(storedCount);
+    }
+  }, [])
+
+  // On mount/clickCount change set count value in local storage
+  useEffect(() => {
+    localStorage.setItem("count", clickCount)
+  }, [clickCount])
 
   return (
     <div className="App">
@@ -12,8 +27,13 @@ function App() {
       </header>
 
       <div className="clickCounter-container">
-        <p>Count: {clickCount}</p>
-        <button id='incrimentButton'>Incriment Count</button>
+        <div className='clickCounter-main'>
+          <p>Count: {clickCount}</p>
+          <div className='clickCounter-actions'>
+            <button onClick={() => setClickCount(0)}>Reset</button>
+            <button onClick={() => setClickCount(clickCount + 1)} id='incrimentButton'>Incriment Count</button>
+          </div>
+        </div>
       </div>
     </div>
   );
